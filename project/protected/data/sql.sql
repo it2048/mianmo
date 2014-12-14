68cd08f7b8361a2863bb4b12e8eb75be8926fe59
jsjscool <jsjscool@163.com>
2014-12-14 13:07:00 +0800

    sql

---
+++ project/protected/data/sql.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS `rs_admin` (
+  `username` varchar(24) NOT NULL COMMENT '用户名',
+  `password` varchar(32) NOT NULL COMMENT '密码',
+  `tel` varchar(16) DEFAULT NULL COMMENT '电话',
+  `email` varchar(32) DEFAULT NULL COMMENT 'email',
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `rs_admin`
+--
+
+INSERT INTO `rs_admin` (`username`, `password`, `tel`, `email`) VALUES
+('admin', '21232f297a57a5a743894a0e4a801fc3', '18228041351', 'jsjscool@163.com'),
+('xiongfanglei', '4297f44b13955235245b2497399d7a93', '123456', '123456@163.com');
+
+CREATE TABLE IF NOT EXISTS `rs_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `time` int(11) NOT NULL COMMENT '创建时间',
+  `ip` varchar(16) NOT NULL COMMENT '用户IP',
+  `number` int(11) NOT NULL COMMENT '数量',
+  `money` varchar(64) NOT NULL COMMENT '钱',
+  `beizhu` text NOT NULL COMMENT '备注',
+  `zone` varchar(128) NOT NULL COMMENT '备注',
+  `address` varchar(1024) NOT NULL COMMENT '备注',
+  `name` varchar(64) NOT NULL COMMENT '备注',
+  `mobilephone` varchar(32) NOT NULL COMMENT '备注',
+  `postcode` varchar(32) NOT NULL COMMENT '备注',
+  `phone` varchar(32) NOT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='订单表' AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `rs_order`
+--
+
+INSERT INTO `rs_order` (`id`, `time`, `ip`, `number`, `money`, `beizhu`, `zone`, `address`, `name`, `mobilephone`, `postcode`, `phone`) VALUES
+(2, 1418490212, '127.0.0.1', 2, '298.00', '请在此填写您对订单或商品的特殊要求或说明，最多300字', '北京', '625 Jersey Ave Unit 8', 'JSJSCOOL CFTM', '18228041350', '08901', '7326131400');
+
