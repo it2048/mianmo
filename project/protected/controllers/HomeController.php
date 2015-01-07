87471243eed818f8b344a5d842db0320ab682945
jsjscool <jsjscool@163.com>
2015-01-07 11:28:08 +0800

    ces

--- project/protected/controllers/HomeController.php
+++ project/protected/controllers/HomeController.php
@@ -38,67 +38,134 @@ class HomeController extends Controller
     
     public function actionAlinotify()
     {
-        echo 123;
+        //计算得出通知验证结果
+        $alipayNotify = new AlipayNotify(Yii::app()->params->alipay_config);
+        $verify_result = $alipayNotify->verifyNotify();
+
+        if($verify_result) {
+            //商户订单号
+            $out_trade_no = $_POST['out_trade_no'];
+
+            //支付宝交易号
+            $trade_no = $_POST['trade_no'];
+
+            //交易状态
+            $trade_status = $_POST['trade_status'];
+
+
+            if($trade_status == 'TRADE_FINISHED') {
+
+            }
+            else if ($trade_status == 'TRADE_SUCCESS') {
+                $aid = AppRsOrder::model()->findByPk($out_trade_no);
+                if(!empty($aid->mobilephone))
+                {
+                    $aid->pay_status = 2;
+                    $aid->save();
+                }
+            }
+            echo "success";		//请不要修改或删除
+        }
+        else {
+            //验证失败
+            echo "fail";
+        }
     }
     public function actionAlireturn()
     {
-        echo 456;
+        $this->nav="success";
+        $home = Yii::app()->request->baseUrl."/public/home/";
+        $app = null;
+        //计算得出通知验证结果
+        $alipayNotify = new AlipayNotify(Yii::app()->params->alipay_config);
+        $verify_result = $alipayNotify->verifyReturn();
+        if($verify_result) {//验证成功
+
+            $out_trade_no = $_GET['out_trade_no'];
+
+            //支付宝交易号
+
+            $trade_no = $_GET['trade_no'];
+
+            //交易状态
+            $trade_status = $_GET['trade_status'];
+
+            if($trade_status == 'TRADE_SUCCESS') {
+                $aid = AppRsOrder::model()->findByPk($out_trade_no);
+                if(!empty($aid->mobilephone))
+                {
+                    $aid->pay_status = 2;
+                    $aid->save();
+                    $app = $aid;
+                }
+            }
+        }
+        $this->renderPartial('success',array("home"=>$home,"arr"=>$app,"type"=>""));
     }
     public function actionAlipay()
     {
-        //支付类型
-        $payment_type = "1";
-        //必填，不能修改
-        //服务器异步通知页面路径
-        $notify_url = Yii::app()->createAbsoluteUrl('home/Alinotify');
-        //需http://格式的完整路径，不能加?id=123这类自定义参数        //页面跳转同步通知页面路径
-        $return_url = Yii::app()->createAbsoluteUrl('home/Alireturn');
-        //需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/  
-        //卖家支付宝帐户
-        $seller_email = '18228041350';
-        //必填        
-        //商户订单编号
-        $out_trade_no = "2";
-        //商户网站订单系统中唯一订单号，必填        //订单名称
-        $subject = "牛逼的面膜";
-        //必填        //付款金额
-        $total_fee = "付款金额";
-        //必填        //订单描述
-        $body = "订单的一些描述";
-        //商品展示地址
-        $show_url = "商品的地址";
-        //需以http://开头的完整路径，例如：http://www.商户网址.com/myorder.html        
-        //防钓鱼时间戳
-        $anti_phishing_key = "";
-        //若要使用请调用类文件submit中的query_timestamp函数        //客户端的IP地址
-        $exter_invoke_ip = "用户下单的ip";
-        //非局域网的外网IP地址，如：221.0.0.1
+        $money = Yii::app()->getRequest()->getParam("url", ""); //总价
+        $arr = json_decode(base64_decode($money));
+        if(is_array($arr))
+        {
+            $aid = AppRsOrder::model()->findByPk($arr[0]);
+            if($aid->mobilephone == $arr[1])
+            {
+                //支付类型
+                $payment_type = "1";
+                //必填，不能修改
+                //服务器异步通知页面路径
+                $notify_url = Yii::app()->createAbsoluteUrl('home/Alinotify');
+                //需http://格式的完整路径，不能加?id=123这类自定义参数        //页面跳转同步通知页面路径
+                $return_url = Yii::app()->createAbsoluteUrl('home/alireturn');
+                //需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
+                //卖家支付宝帐户
+                $seller_email = '654805289@qq.com';
+                //必填
+                //商户订单编号
+                $out_trade_no = $aid->id;
+                //商户网站订单系统中唯一订单号，必填        //订单名称
+                $subject = "showplus面膜";
+                //必填        //付款金额
+                $total_fee = $aid->money;
+                //必填        //订单描述
+                $body = $aid->beizhu;
+                //商品展示地址
+                $show_url = "http://www.showplus.cc";
+                //需以http://开头的完整路径，例如：http://www.商户网址.com/myorder.html
+                //防钓鱼时间戳
+                $anti_phishing_key = "";
+                //若要使用请调用类文件submit中的query_timestamp函数        //客户端的IP地址
+                $exter_invoke_ip = $aid->ip;
+                //非局域网的外网IP地址，如：221.0.0.1
 
+                /************************************************************/
 
-        /************************************************************/
-
-        //构造要请求的参数数组，无需改动
-        $parameter = array(
-                        "service" => "create_direct_pay_by_user",
-                        "partner" => trim(Yii::app()->params->alipay_config['partner']),
-                        "payment_type"	=> $payment_type,
-                        "notify_url"	=> $notify_url,
-                        "return_url"	=> $return_url,
-                        "seller_email"	=> $seller_email,
-                        "out_trade_no"	=> $out_trade_no,
-                        "subject"	=> $subject,
-                        "total_fee"	=> $total_fee,
-                        "body"	=> $body,
-                        "show_url"	=> $show_url,
-                        "anti_phishing_key"	=> $anti_phishing_key,
-                        "exter_invoke_ip"	=> $exter_invoke_ip,
-                        "_input_charset"	=> trim(strtolower(Yii::app()->params->alipay_config['input_charset']))
-        );
-
-        //建立请求
-        $alipaySubmit = new AlipaySubmit(Yii::app()->params->alipay_config);
-        $html_text = $alipaySubmit->buildRequestForm($parameter,"get", "确认");
-        echo $html_text;
+                //构造要请求的参数数组，无需改动
+                $parameter = array(
+                    "service" => "create_direct_pay_by_user",
+                    "partner" => trim(Yii::app()->params->alipay_config['partner']),
+                    "payment_type"	=> $payment_type,
+                    "notify_url"	=> $notify_url,
+                    "return_url"	=> $return_url,
+                    "seller_email"	=> $seller_email,
+                    "out_trade_no"	=> $out_trade_no,
+                    "subject"	=> $subject,
+                    "total_fee"	=> $total_fee,
+                    "body"	=> $body,
+                    "show_url"	=> $show_url,
+                    "anti_phishing_key"	=> $anti_phishing_key,
+                    "exter_invoke_ip"	=> $exter_invoke_ip,
+                    "_input_charset"	=> trim(strtolower(Yii::app()->params->alipay_config['input_charset']))
+                );
+                //建立请求
+                $alipaySubmit = new AlipaySubmit(Yii::app()->params->alipay_config);
+                $html_text = $alipaySubmit->buildRequestForm($parameter,"get", "确认");
+                echo $html_text;
+                die();
+            }
+        }
+        echo "非法访问";
     }
     public function actionBuy()
     {
@@ -227,7 +294,10 @@ class HomeController extends Controller
                 $aid->save();
             }
             $this->msgsucc($msg);
-            $msg['data'] = Yii::app()->createAbsoluteUrl('home/success')."/type/".$pay_way."/url/".$money; 
+            if($pay_way==2)
+                $msg['data'] = Yii::app()->createAbsoluteUrl('home/success')."/type/".$pay_way."/url/".$money;
+            else
+                $msg['data'] = Yii::app()->createAbsoluteUrl('home/alipay')."/type/".$pay_way."/url/".$money;
         }
         echo json_encode($msg);
     }
