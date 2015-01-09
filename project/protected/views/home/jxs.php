0adf8875d3069ddcca32d23d9fb2a65d6ce660d6
unknown <chenchunhong@CZD01047.kefu.kingsoft>
2015-01-09 14:42:55 +0800

    经销商查询

--- project/protected/views/home/jxs.php
+++ project/protected/views/home/jxs.php
@@ -3,7 +3,7 @@
     <div class="jxs">
         <div class="jxs_input">
             <input type="text" placeholder="请输入经销商代码" class="jxs_search">
-            <input type="submit" class="jxs_submit" value="" title="查询">
+            <input type="text" class="jxs_submit" value="" title="查询" onclik="submitJxs('<?php echo Yii::app()->createAbsoluteUrl('home/chk'); ?>')">
         </div>
     </div>
 </div>
