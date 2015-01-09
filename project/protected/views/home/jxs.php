9f5928ba6ae84670c9a5c126b1a8905c1d5d64e8
unknown <chenchunhong@CZD01047.kefu.kingsoft>
2015-01-09 14:59:45 +0800

    dsas

--- project/protected/views/home/jxs.php
+++ project/protected/views/home/jxs.php
@@ -9,7 +9,11 @@
 </div>
  <script type="text/javascript">
    $(".jxs_submit").click(function(){
+   	    if(!$.trim($(".jxs_search").val()){
+   	    	alert("请输入经销商代码！");
+   	    }else{
                       searchJxs('<?php echo Yii::app()->createAbsoluteUrl('home/chk'); ?>');
+                  }
                     });
  </script>
  
